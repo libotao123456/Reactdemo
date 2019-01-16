2941f74646b3cf6a6bb170e5f8d0f29c7317c2ae
libotao <915004581@qq.com>
2019-01-16 11:03:06 +0800

    react练习demo

---
+++ src/Comment.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class Comment extends Component {
+  render() {
+    return (
+      <div className='comment'>
+        <div className='comment-user'>
+          <span>{this.props.comment.username}</span> :
+        </div>
+        <p>&nbsp;{this.props.comment.content}</p>
+      </div>
+    )
+  }
+}
+export default Comment
